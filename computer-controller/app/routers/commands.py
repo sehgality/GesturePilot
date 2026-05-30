2e866102d655e433609975d5277d3f6902900216
Aidan <aidan.sehgal21@gmail.com>
2026-05-30 12:56:19 -0700

    added pyautogui commands

--- computer-controller/app/routers/commands.py
+++ computer-controller/app/routers/commands.py
@@ -1,9 +1,22 @@
 from fastapi import APIRouter
+import pyautogui
+from enum import Enum
 
 router = APIRouter()
 
+class Command(str, Enum):
+    UP = "up"
+    DOWN = "down"
+    LEFT = "left"
+    SPACE = "space"
+    RIGHT = "right"
+
+
 @router.get("/commands/{command}", tags=["command"])
-async def read_user(command: str):
-    print(f"{command}")
+async def read_user(command: Command):
+    pyautogui.press(command.value)
+
+
+
 
     return {"command": command}
